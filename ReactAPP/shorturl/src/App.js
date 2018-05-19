a8b7dc6cc2540ca0f073f3e9bc516fa5846a60d4
sultanshaik <sultanshaik.dev@gmail.com>
2018-05-19 15:52:11 -0400

    Add REST calls to parent container

--- ReactAPP/shorturl/src/App.js
+++ ReactAPP/shorturl/src/App.js
@@ -7,16 +7,60 @@ class App extends Component {
   constructor()
   {
     super();
-    this.state = {listofUrls:{}, Urltoshorten:''}
+    this.state = {listofUrls:[], Urltoshorten:''}
+  }
+
+  filter(x){
+  return	x;
+}
+
+  fetchUrls(){
+
+    fetch("http://localhost:8080/urls").
+          then(response=>response.json()).
+              then(response =>
+                {
+                  this.setState(
+                      {listofUrls : this.filter(response)}
+                )
+              }).catch(function(error) {
+  });
+}
+
+  componentWillMount()
+  {
+      this.fetchUrls();
   }
 
   handleChange =(e)=>
   {
     this.setState({Urltoshorten : e.target.value}, ()=>
-    {
-      console.log(this.state.Urltoshorten)
-    }
-  );
+      {
+        console.log(this.state.Urltoshorten)
+      }
+    );
+  }
+
+  addNewUrl()
+  {
+        var initvalues = {
+            method: 'POST',
+            headers: {
+                    'Content-Type': 'application/json'
+                  },
+            body:    this.state.Urltoshorten
+          }
+          fetch("http://localhost:8080/urls" ,initvalues)
+          .then(this.fetchUrls())
+          .catch(error=>{});
+
+
+  }
+
+  handleSubmit = (e)=>
+  {
+      e.preventDefault();
+      this.addNewUrl();
   }
 
   render() {
@@ -27,7 +71,7 @@ class App extends Component {
 		            placeholder="Url to shorten" value = {this.state.Urltoshorten} onChange = {this.handleChange} />
 		<button className="" onClick = {this.handleSubmit}  >Add Url</button>
 		</form>
-    <ListComponent />
+    <ListComponent listofUrls = {this.state.listofUrls} />
 	  </div>
     );
   }
--- ReactAPP/shorturl/src/ListComponent.js
+++ ReactAPP/shorturl/src/ListComponent.js
@@ -5,31 +5,15 @@ class ListComponent extends Component{
     constructor(props)
     {
       super(props);
-      this.state = {listofUrls : []}
-
     }
 
-    filter(x){
-		return	x;
-	}
-
-    fetchUrls(){
-
-      fetch("http://localhost:8080/urls").
-            then(response=>response.json()).
-                then(response =>
-                  {
-                    this.setState(
-                        {listofUrls : this.filter(response)}
-                  )
-                }).catch(function(error) {
-    });
-}
-
-    componentDidMount(){
-
-		   this.fetchUrls();
-	}
+    componentWillReceiveProps(nextProps)
+    {
+      if(nextProps.listofUrls!==this.props.listofUrls)
+      {
+        this.props = nextProps;
+      }
+    }
 
     render() {
       return (
@@ -42,16 +26,17 @@ class ListComponent extends Component{
   </tr>
   </thead>
   <tbody>
-  {this.state.listofUrls.map((x,i)=>{
-    return(
-      <tr key={i}>
-        <td>{x.actualURL}</td>
-        <td>{x.shortURL}</td>
-      </tr>
-      )
-    }
-  )
-  }
+  {
+    this.props.listofUrls.map((x,i)=>{
+          return(
+                  <tr key={i}>
+                  <td>{x.actualURL}</td>
+                  <td>{x.shortURL}</td>
+                  </tr>
+                )
+          }
+  )}
+
   </tbody>
    </table>
 
